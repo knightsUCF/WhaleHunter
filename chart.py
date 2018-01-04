222f3863da806229a1dcb3c9b53176155fe4cbd6
Peter Kwiatkowski <peterk@knights.ucf.edu>
2018-01-03 21:28:54 -0500

    Update chart.py

--- chart.py
+++ chart.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python3
+import numpy as np
+from bokeh.charts import Line, output_file, show
+
+# (dict, OrderedDict, lists, arrays and DataFrames are valid inputs)
+xyvalues = np.array([[2, 3, 7, 5, 26], [12, 33, 47, 15, 126], [22, 43, 10, 25, 26]])
+
+line = Line(xyvalues, title="line", legend="top_left", ylabel='Languages')
+
+output_file('line.html')
+show(line)
 
 
-import bokeh
-
-
-
-class Chart():
-
-
-    def placeholder(self):
-        pass
 
 
