429320587d5c3e00c0c95bb3a2cfa40d7d6a7ed3
Peter Kwiatkowski <peterk@knights.ucf.edu>
2018-01-03 21:33:49 -0500

    Update chart.py

--- chart.py
+++ chart.py
@@ -1,5 +1,5 @@
 import numpy as np
-from bokeh.charts import Line, output_file, show
+from bkcharts import Line, output_file, show
 
 # (dict, OrderedDict, lists, arrays and DataFrames are valid inputs)
 xyvalues = np.array([[2, 3, 7, 5, 26], [12, 33, 47, 15, 126], [22, 43, 10, 25, 26]])
