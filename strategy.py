b2ca74fbadb9dc515d2a37d81675d4ea3e205e88
Peter Kwiatkowski <peterk@knights.ucf.edu>
2018-01-22 04:24:03 -0500

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -33,11 +33,26 @@ class Strategy():
         self.price_spike_magnitude_coefficient = user.price_spike_magnitude_coefficient
         self.volume_drop_magnitude_coefficient = user.volume_drop_magnitude_coefficient 
         self.price_drop_magnitude_coefficient = user.price_drop_magnitude_coefficient
+
+        self.close_from_the_top_percentage = user.close_from_the_top_percentage
+
         self.all_coin_data = broker.get_data_for_all_coins('bittrex') # used to get all the symbols
         self.queue = 'free'
         self.can_trade = 'yes'
 
-        self.starting_btc_balance = 1.0
+        self.btc_paper_balance = 1.0
+        self.paper_trade_open_price = 0.0
+        self.old_altcoin_price = 0
+        self.new_altcoin_price = 0
+
+        self.last_traded_pair = ''
+
+        self.all_time_high = 0
+
+        # write out strategy settings for log:
+        text = 'Strategy settings: ' + ' spike percent coefficient: ' + str(self.price_spike_magnitude_coefficient) + ' exit price drop coefficient: ' + str(self.price_drop_magnitude_coefficient) 
+        txt.write_next_line('paper_trading', text)
+
 
 
 
@@ -100,7 +115,7 @@ class Strategy():
 
 
     def drop_in_price(self, broker, pair):
-        if self.current_price(broker, pair) > (self.average_price(broker, pair, self.exit_lookback) * self.price_drop_magnitude_coefficient):
+        if self.current_price(broker, pair) < (self.average_price(broker, pair, self.exit_lookback) * self.price_drop_magnitude_coefficient):
             return True
         else:
             return False
@@ -129,13 +144,16 @@ class Strategy():
                 if self.spike_in_price('bittrex', symbol):
                     print('\nFound a pair which meets entry criteria')
                     self.current_trade_pair = symbol
-                    # break
+                    return True
+                    # break # IMPORTANT -- break loop for now and go with the first one, later can filter more choices, perhaps by volume
                     '''
                     return True
                 else:
                     return False
                     '''
     
+
+
     def get_current_trade_pair(self):
         return self.current_trade_pair
             
@@ -143,10 +161,20 @@ class Strategy():
 
 
     def exit_conditions(self, coin):
+        broker = 'bittrex'
+        current_price = self.current_price(broker, coin)
+        if current_price > self.all_time_high:
+            self.all_time_high = current_price
+        if current_price < self.all_time_high * self.close_from_the_top_percentage:
+            return True
+        else:
+            return False
+        '''
         if self.drop_in_price('bittrex', coin):
             return True
         else:
             return False
+            '''
             
 
     '''
@@ -183,8 +211,42 @@ class Strategy():
 
 
 
+    def paper_trade(self):
+        broker = 'bittrex'
+        print('\nQueue status: ', self.queue, '\n')
+        if self.queue is 'free':
+            if self.entry_conditions():
+                print('reached entry conditions')
+                self.last_traded_pair = self.current_trade_pair
+                self.old_altcoin_price = self.current_price(broker, self.last_traded_pair)
+                text_to_write = 'Open: ' + str(self.current_trade_pair) + ' at ' + str(self.old_altcoin_price)
+                txt.write_next_line('paper_trading', text_to_write)
+                balance_to_write = 'BTC paper balance: ' + str(self.btc_paper_balance)
+                txt.write_next_line('paper_trading', balance_to_write)
+                self.queue = 'busy'
+        if self.queue is 'busy':
+            if self.exit_conditions(self.current_trade_pair):
+                print('reached exit conditions')
+                # trade.close('bittrex', self.current_trade_pair) # get back into bitcoin
+                self.new_altcoin_price = self.current_price(broker, self.last_traded_pair)
+                close_text_to_write = 'Close: ' + str(self.new_altcoin_price)
+                txt.write_next_line('paper_trading', close_text_to_write)
+                bitcoin_price = self.current_price('bittrex', 'USDTBTC')
+                self.btc_paper_balance = self.btc_paper_balance + (self.new_altcoin_price - self.old_altcoin_price) * bitcoin_price
+                balance_text = 'New balance: ' + str(self.btc_paper_balance)
+                txt.write_next_line('paper_trading', balance_text)
+                # self.updated_balance = self.paper_trade_open_price # need to do the conversion in BTC
+                self.queue = 'free' # reset the queue
+                self.all_time_high = 0 # reset all time high because will be different for different coins
+
+
+
+
     def run(self):
         self.trade()
+
+
+
 
 
 
